01f722522aa6f2d96668fe3486abe064db18cdbf
Ben <me@benl.dev>
2023-11-18 19:48:22 -0500

    small cleanup

--- personal/install.sh
+++ personal/install.sh
@@ -1,7 +1,6 @@
 # Custom actions to take on initial install of dotfiles.
 # This runs after default install actions, so you can overwrite changes it makes if you want.
 
-
 # install exa for better ls
 if [ $SPIN ]; then
   sudo apt-get install -y exa
