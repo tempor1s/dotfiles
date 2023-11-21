f10cc39a2e46b0879922136c12296dd51f6a9ef3
Ben Lafferty <ben.lafferty@shopify.com>
2023-11-20 23:47:21 -0500

    add bat on mac

--- personal/macos.zsh
+++ personal/macos.zsh
@@ -5,4 +5,7 @@
 [[ -f /opt/dev/sh/chruby/chruby.sh ]] && type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
 [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
 
+# replace cat with bat
+alias cat='bat --paging=never'
+
 eval "$(github-copilot-cli alias -- "$0")"
