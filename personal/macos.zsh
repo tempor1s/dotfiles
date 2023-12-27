550f7f2168d012afe86a22e6f37930df97a46614
Ben <me@benl.dev>
2023-12-27 11:16:18 -0700

    use homebrew ruby on macos if it exists

--- personal/macos.zsh
+++ personal/macos.zsh
@@ -1,6 +1,12 @@
 # Custom configs for MacOS environments.
 # This file will only be executed if the current environment is MacOS.
 
+# use homebrew ruby if it exists
+if [ -d "/opt/homebrew/opt/ruby/bin" ]; then
+  export PATH=/opt/homebrew/opt/ruby/bin:$PATH
+  export PATH=`gem environment gemdir`/bin:$PATH
+fi
+
 # setup dev
 [[ -f /opt/dev/sh/chruby/chruby.sh ]] && type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
 [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
