03961eee6fb7bbd7b2a55cf3e1f7dea76ca11e61
Ben <me@benl.dev>
2024-02-29 12:52:24 -0700

    more vim config, will prob move to lazy soon though

--- personal/configs/nvim/lua/configs/packer.lua
+++ personal/configs/nvim/lua/configs/packer.lua
@@ -6,23 +6,20 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim'
 
-  -- For finding files (requires ripgrep)
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-
-  -- For nice syntax highlighting
   use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = ':TSUpdate'
+	  "ThePrimeagen/harpoon",
+	  branch = "harpoon2",
+	  requires = { {"nvim-lua/plenary.nvim"} }
   }
-
-  -- Theme!
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-  })
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 end)
