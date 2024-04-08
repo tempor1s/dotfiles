ed3a995e5f9fd5cc6d55f5184cc7196d284bf1da
Ben Lafferty <ben.lafferty@shopify.com>
2024-04-08 16:13:39 -0400

    install eza in spin

--- personal/install.sh
+++ personal/install.sh
@@ -1,10 +1,20 @@
 # Custom actions to take on initial install of dotfiles.
 # This runs after default install actions, so you can overwrite changes it makes if you want.
 
+# function to install eza in Spin
+install_eza_in_spin() {
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
+}
+
 # install deps!
 if [ $SPIN ]; then
   sudo apt-get install -y bat
-  sudo apt-get install -y exa
+  install_eza_in_spin
   sudo apt-get install -y neovim
   sudo apt-get install -y ripgrep
   sudo apt-get install -y tmux
