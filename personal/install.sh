d0c3c6e5adec3278018b6c5e8e8a17980868df37
Ben <me@benl.dev>
2024-06-08 02:20:11 -0400

    lazygit, nvim config, increase font size

--- personal/install.sh
+++ personal/install.sh
@@ -3,28 +3,32 @@
 
 # function to install eza in Spin
 install_eza_in_spin() {
-  sudo mkdir -p /etc/apt/keyrings
-  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-  sudo apt update
-  sudo apt install -y eza
+	sudo mkdir -p /etc/apt/keyrings
+	wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+	sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+	sudo apt update
+	sudo apt install -y eza
 }
 
 # install deps!
 if [ $SPIN ]; then
-  sudo apt-get install -y bat
+	sudo apt-get install -y bat
+	sudo apt-get install -y neovim
+	sudo apt-get install -y ripgrep
+	sudo apt-get install -y tmux
+	sudo apt-get install -y zellij
+	sudo apt-get install -y fd
+  sudo apt-get install -y lazygit
   install_eza_in_spin
-  sudo apt-get install -y neovim
-  sudo apt-get install -y ripgrep
-  sudo apt-get install -y tmux
-  sudo apt-get install -y zellij
 else
-  brew install eza
-  brew install bat
-  brew install neovim
-  brew install ripgrep
-  brew install zellij
+	brew install eza
+	brew install bat
+	brew install neovim
+	brew install ripgrep
+	brew install zellij
+	brew install fd
+  brew install jesseduffield/lazygit/lazygit
 fi
 
 # link in neovim configs
