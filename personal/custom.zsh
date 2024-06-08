1d94c4ebbd3b7f1b8c5e6376f5b9a0fc9a984fc2
Ben <me@benl.dev>
2024-06-08 01:46:34 -0400

    fix zellij config and base lazyvim config

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -3,14 +3,13 @@
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
 
-# replace ls with exz
 alias l="eza"
 alias ls="eza"
 alias ll="eza -l"
 alias la="eza -la"
 
-# replace vim with nvim
 alias vim="nvim"
+alias vi="nvim"
 
 # spin shortcuts
 alias sus="spin up sidekick:web"
--- personal/nvim/init.lua
+++ personal/nvim/init.lua
@@ -1,1 +1,2 @@
-require("personal")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
